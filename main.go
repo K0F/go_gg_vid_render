8c8b1f79ececd7341389442314cb7f6691b42dc6
Krystof Pesek <krystof.pesek@gmail.com>
2023-02-12 23:41:34 +0100

    fmt

--- main.go
+++ main.go
@@ -1,45 +1,35 @@
-
 package main
-
 
 import "github.com/fogleman/gg"
 import "fmt"
 import "math"
 
 var frameCount = 0
-var noFrames = 25*100
+var noFrames = 25 * 100
 
 func main() {
-    dc := gg.NewContext(1920, 1080)
-    
+	dc := gg.NewContext(1920, 1080)
 
+	for frameCount < noFrames {
+		// background
+		dc.DrawRectangle(0, 0, 1920, 1080)
+		dc.SetRGB(0, 0, 0)
+		dc.Fill()
 
-    for frameCount < noFrames {
-    
-    // background
-    dc.DrawRectangle(0,0,1920,1080)
-    dc.SetRGB(0,0,0)
-    dc.Fill()
-   
+		i := 1
+		for i < 100 {
+			val := math.Sin(float64(frameCount)/float64(noFrames)/float64(i)*math.Pi*2) / 2.0
+			i++
+			dc.DrawCircle(1920/2+val*400, 1080/2, 400-float64(i)*4)
+			dc.SetRGB(1, 1, 1)
+			dc.Stroke()
+		}
+		// dc.Fill()
 
-    i:=1
-    for i < 100 {
-    val := math.Sin(float64(frameCount)/float64(noFrames)/float64(i)*math.Pi*2)/2.0
-    i++
-    dc.DrawCircle(1920/2 + val*400, 1080/2, 400 - float64(i) * 4)
-    
-    dc.SetRGB(1,1,1)
-    dc.Stroke()
-  }
-   // dc.Fill()
+		filename := fmt.Sprintf("%05d_out.png", frameCount)
+		fmt.Println("Saving: " + filename)
+		dc.SavePNG(filename)
 
-
-
-
-    filename := fmt.Sprintf("%05d_out.png",frameCount)
-    fmt.Println("Saving: "+filename)
-    dc.SavePNG(filename)
-
-    frameCount++
-  }
+		frameCount++
+	}
 }
